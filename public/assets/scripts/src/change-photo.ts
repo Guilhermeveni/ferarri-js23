0848c586f334060bcffc18b00bafa35093fff75d
Guilherme Magalhães <98272340+Guilhermeveni@users.noreply.github.com>
2023-03-28 08:44:37 -0300

    .

--- public/assets/scripts/src/change-photo.ts
+++ public/assets/scripts/src/change-photo.ts
@@ -1,5 +1,5 @@
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { v4 as uuidv4 } from 'uuid';
+//import { v4 as uuidv4 } from 'uuid';
 import { getAuth, updateProfile, onAuthStateChanged } from "firebase/auth";
 
 const auth = getAuth();
